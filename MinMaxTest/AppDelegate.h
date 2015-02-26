1788ef117e605dd7fb5f1195c9c7da78b9be5ec8
ParkGwangBeom <yellomobile@yellomobileui-iMac.local>
2015-02-26 15:26:13 +0900

    Initial Commit

---
+++ MinMaxTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MinMaxTest
+//
+//  Created by yellomobile on 2015. 2. 26..
+//  Copyright (c) 2015년 yellomobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
